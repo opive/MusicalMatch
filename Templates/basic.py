f9d8f26abe3fbe636072d563aede6d123c61f643
opive <160372667+opive@users.noreply.github.com>
2024-04-20 21:35:32 -0400

    Add files via upload

---
+++ Templates/basic.py
@@ -0,0 +1,7 @@
+import os 
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+print(basedir)
---
+++ Templates/setupdatabase.py
@@ -0,0 +1,19 @@
+from basic import db,Puppy
+
+db.create_(all)
+
+sam = Puppy('Sammy',3)
+frank = Puppy('Frankie',4)
+
+print(sam.id)
+print(frank.id)
+
+db.session.add_all([sam,frank])
+
+
+db.session.commit()
+
+print(sam.id)
+print(frank.id)
+
+
